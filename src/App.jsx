e5d61ba2fd88f2253a38cc52a95b94dc7c0c9568
Ale <alearanibar@gmail.com>
2020-11-07 23:30:13 -0800

    api renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
+import Row from './components/row'
+import requests from './utils/request'
 
 function App() {
+
+
+
   return (
     <div className="App">
         <h1>Netflix Clone</h1> 
+        <Row title='NETLFIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} />
+        <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+        
     </div>
   );
 }
